e28bad64a1aad0e50682e1538bc9f2476f874a0d
Gowhara <yasmiin.21eid@gmail.com>
2024-02-18 22:36:22 +0200

    new changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import Logo from '../../assets/logo.png';
 import './NavBar.css';
 const NavBar = () => {
     return (
        <div className="navbar">
         <div className="left">
-        <img src={Logo} alt="" />
+       {/* <img src={Logo} alt="" />*/}
         </div>
         <div className="right">
             <div className="item">Home</div>
-            <div className="item">About Us</div>
-            <div className="item">Community</div>
-            <div className="item">Pricing</div>
+            <div className="item">About Me</div>
+            <div className="item">Services</div>
+            <div className="item">Contact</div>
         </div>
            
 
